66e9edf6ce22f5fcaab6df811ba8759db05607b8
radiohonker <123379591+radiohonker@users.noreply.github.com>
2023-10-31 13:32:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 const multer = require('multer');
 
 let cors = require('cors');
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: 'http://localhost:3005' }));
 
 const book = require("./models/books");
 const account = require("./models/accounts");
