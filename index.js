3f35f314de08b005cd2401b1b79ceafc02df99ad
radiohonker <123379591+radiohonker@users.noreply.github.com>
2023-10-31 13:09:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let express = require(`express`);
 let app = express();
-let port = 3000;
+let port = process.env.PORT || 3000;
 let mongoose = require('mongoose');
 require('dotenv').config()
 async function com() {
