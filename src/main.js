eb5638b8b385727579e163e74604a4e699780839
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-02-02 22:07:28 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Запросы к серверу
 import axios from "axios";
-axios.defaults.baseURL = 'https://litclub-5pbp.onrender.com';
+axios.defaults.baseURL = 'https://troubled-teal-caiman.cyclic.app/';
 
 // Работа с датой
 import dayjs from 'dayjs';
