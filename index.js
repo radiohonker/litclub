2eaf45cc8ed543d3c61f1af5fc2049c11f794212
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-02-02 23:18:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,12 @@ const cors = require('cors');
 const corsOptions ={
     origin:['http://localhost:5173','http://127.0.0.1:5173'],
     credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    optionSuccessStatus:200,
+    res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
 }
 app.use(cors(corsOptions));
 
