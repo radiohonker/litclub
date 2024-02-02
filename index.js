55cbe7f7eae0d30a35a79e3ef720162c7532ba0a
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-02-02 23:14:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const multer = require('multer');
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'https://lonely-kimono-eel.cyclic.app', 
+    origin:'https://localhost:${PORT}', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
