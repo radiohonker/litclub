68c00248da07086f33bd8133e88a67cda2f00d08
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-02-02 23:18:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,17 +32,16 @@ app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.use(express.json());
 const multer = require('multer');
-
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
 const cors = require('cors');
 const corsOptions ={
     origin:['http://localhost:5173','http://127.0.0.1:5173'],
     credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-    res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+    optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
 
