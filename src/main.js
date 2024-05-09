39a0ffeef6554dddd70db74596cd38b49a9f3a07
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-05-09 19:16:56 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Запросы к серверу
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3005';
+axios.defaults.baseURL = 'http://89.111.131.69:5000';
 
 // Работа с датой
 import dayjs from 'dayjs';
