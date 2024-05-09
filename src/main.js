b777e65d62a55a294b94deedf745125df525b7c8
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-05-09 21:07:14 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Запросы к серверу
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3005/';
+axios.defaults.baseURL = 'http://litclub-project.online:3005/';
 
 // Работа с датой
 import dayjs from 'dayjs';
