0c34d24876d6b7caeeee6fce72a55dc004f6500e
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-05-09 18:30:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,12 @@ const multer = require('multer');
 
 const cors = require('cors');
 app.use(cors({ origin: 'http://localhost:8080' }));
-
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 const book = require("./models/books");
 const account = require("./models/accounts");
 
