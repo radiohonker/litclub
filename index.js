7969b8b07dd7b313edcedc2d799cccb0f57f0593
radiohonker <123379591+radiohonker@users.noreply.github.com>
2024-05-09 17:39:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,34 +28,12 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(bodyParser.json({limit: '10mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://litclub-rho.vercel.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 app.use(express.json());
 const multer = require('multer');
 
 const cors = require('cors');
-const corsOptions ={
-    origin:['https://litclub-rho.vercel.app/']
-}
-app.use(cors(corsOptions));
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 const book = require("./models/books");
 const account = require("./models/accounts");
